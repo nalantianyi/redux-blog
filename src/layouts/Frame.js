26ac2a93e4604af7c0022beec9e507ed1623b9e2
纳兰天忆 <103520146@qq.com>
2016-12-20 16:24:06 +0800

    update

---
+++ src/layouts/Frame.js
@@ -0,0 +1,19 @@
+/**
+ * Created by nalantianyi on 2016/12/20.
+ */
+import React, {Component} from 'react';
+import Nav from './Nav';
+export default class Frame extends Component {
+    render() {
+        return (
+            <div className="frame">
+                <section className="header">
+                    <Nav/>
+                </section>
+                <section className="container">
+                    {this.props.children}
+                </section>
+            </div>
+        );
+    }
+}
---
+++ src/layouts/Nav.js
@@ -0,0 +1,15 @@
+/**
+ * Created by nalantianyi on 2016/12/20.
+ */
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+export default class Nav extends Component {
+    render(){
+        return(
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        );
+    }
+}
